4d07cd6297ffe748f0404f482b2f7fcb02b655eb
LyMarita <lymarita19@gmail.com>
2016-11-25 13:46:50 +0700

    complete category

--- News/app/controllers/categories_controller.rb
+++ News/app/controllers/categories_controller.rb
@@ -1,2 +1,43 @@
 class CategoriesController < ApplicationController
+	def index
+		@show_category=Category.all	
+	end
+	def show
+		@category=Category.find(params[:id])
+	end	
+	def edit
+		
+		@category=Category.find(params[:id])
+	end
+
+	def update
+		
+		@category=Category.find(params[:id])
+		@category.update(category_params)
+		redirect_to categories_url
+	end	
+
+	def new
+		
+		@category=Category.new
+	end	
+	def create
+		#@show_category=Category.all
+		@category=Category.new(category_params)
+		if @category.save
+			redirect_to categories_url 
+		else
+			render :category
+		end	
+	end	
+
+	def destroy
+		@category=Category.find(params[:id])
+		@category.destroy
+		redirect_to categories_url
+	end
+	def category_params
+		params.require(:category).
+					permit(:cate_name)
+	end
 end
--- News/config/routes.rb
+++ News/config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'news' => 'news#index'
-resources :categories
+ # get "events" => "events#index" , as: "all_movies"# gets "url" => "controller_name#module_name"
+ #  get "events/new" => "events#new", as: "movies"
+ #  get "events/:id" => "events#show", as: "movie"
+ #  delete "events/:id" => "events#destroy"
+ #  get "events/:id/edit" => "events#edit", as: "edit_movie" 
+ #  # as: "edit_movie" is equal "events/:id/edit"
+ #  patch "events/:id" => "events#update" 
+ #  post "events/new" => "events#create"
+
+	get 'news' => 'news#index'
+
+	#delete "categories/" =>"categories#show", as: "delete_category"
+	resources :categories
 end
